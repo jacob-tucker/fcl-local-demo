2884ff3008308dd8b7735b3aa46f464ff7f96999
jacob-tucker <jacobtucker818@gmail.com>
2021-07-25 12:14:32 -0400

    added .env.local

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import * as fcl from "@onflow/fcl"
 
 fcl.config()
+  .put("accessNode.api", "http://localhost:8080")
   .put("challenge.handshake", "http://localhost:8701/flow/authenticate")
+// .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
+// .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn")
 
 function App() {
+  const auth = () => {
+    fcl.signUp()
+    console.log("Hey there. I just authenticated.")
+  }
+
   return (
     <div className="App">
-      <button onClick={() => fcl.authenticate()}>Login</button>
+      <button onClick={auth}>Login</button>
     </div>
   );
 }
